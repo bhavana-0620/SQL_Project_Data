a36dee3b6ca221731564be28e50000030a73a88b
bhavana-0620 <bhavanabangaru78@gmail.com>
2026-03-24 19:23:15 +0530

    Updated_top_demanded_skills.sql

--- project_sql/3_top_demanded_skills.sql
+++ project_sql/3_top_demanded_skills.sql
@@ -1,12 +1,9 @@
 /*
 -- 03_top_demanded_skills.sql
 
-Question: What are the most in-demand skills for data analysts?
-- Join job postings to inner join table similar to query 2
-- Identify the top 5 in-demand skills for a data analyst
+Most in-demand skills for data analysts
+- Top 5 in-demand skills for a data analyst
 - Focus on all job postings.
-- Why? Retrieves the top 5 skills with the highest demand in the job market,
-  providing insights into the most valuable skills for job seekers.
 */
 
 WITH filtered_jobs AS (
