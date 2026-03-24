351915324ddcce232056a75b78f62bf204690535
bhavana-0620 <bhavanabangaru78@gmail.com>
2026-03-24 18:58:45 +0530

    Updated_top_paying_job_skills.sql

--- project_sql/2_top_paying_job_skills.sql
+++ project_sql/2_top_paying_job_skills.sql
@@ -2,32 +2,43 @@
 Question: What skills are required for the top-paying data analyst jobs?
 - Use the top 10 highest-paying Data Analyst jobs from first query
 - Add the specific skills required for these roles
-- Why? It provides a detailed look at which high-paying jobs demand certain skills,
-  helping job seekers understand which skills to develop that align with top salaries
 */
 
-with top_paying_jobs AS( 
+WITH ranked_jobs AS (
     SELECT
-    job_id,
-    job_title,
-    salary_year_avg,
-    name as company_name
-from 
-    job_postings_fact
-left JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
-WHERE
-    job_title_short ='Data Analyst' AND
-    job_location ='Anywhere' AND
-    salary_year_avg is not NULL
-ORDER BY
-    salary_year_avg DESC
-LIMIT 10
+        jp.job_id,
+        jp.job_title,
+        jp.salary_year_avg,
+        cd.name AS company_name,
+        ROW_NUMBER() OVER (ORDER BY jp.salary_year_avg DESC) AS rank
+    FROM job_postings_fact jp
+    LEFT JOIN company_dim cd 
+        ON jp.company_id = cd.company_id
+    WHERE
+        jp.salary_year_avg IS NOT NULL
+        AND jp.job_work_from_home = 1
+        AND jp.job_title LIKE '%Data Analyst%'
+        AND jp.job_title NOT LIKE '%Director%'
+        AND jp.job_title NOT LIKE '%Principal%'
+        AND jp.job_title NOT LIKE '%Manager%'
+),
+
+top_paying_jobs AS (
+    SELECT *
+    FROM ranked_jobs
+    WHERE rank <= 10
 )
+
 SELECT 
-    top_paying_jobs.*,
-    skills 
-FROM top_paying_jobs
-INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
-INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+    tpj.job_id,
+    tpj.job_title,
+    tpj.company_name,
+    tpj.salary_year_avg,
+    sd.skills
+FROM top_paying_jobs tpj
+INNER JOIN skills_job_dim sj 
+    ON tpj.job_id = sj.job_id
+INNER JOIN skills_dim sd 
+    ON sj.skill_id = sd.skill_id
 ORDER BY 
-    salary_year_avg DESC
+    tpj.salary_year_avg DESC;
