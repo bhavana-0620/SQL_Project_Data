82023b1362040218bf096b54a674894a5676602b
bhavana-0620 <bhavanabangaru78@gmail.com>
2026-03-25 23:59:28 +0530

    Updated_top_demanded_skills.sql

--- project_sql/3_top_demanded_skills.sql
+++ project_sql/3_top_demanded_skills.sql
@@ -1,36 +1,36 @@
-/*
 -- 03_top_demanded_skills.sql
 
-Most in-demand skills for data analysts
+/*
+Goal: Identify the top 5 most in-demand skills for remote Data Analyst roles.
 - Top 5 in-demand skills for a data analyst
 - Focus on all job postings.
 */
 
-WITH filtered_jobs AS (
-    SELECT job_id
-    FROM job_postings_fact
-    WHERE
-        job_title LIKE '%Data Analyst%'
-        AND job_title NOT LIKE '%Director%'
-        AND job_title NOT LIKE '%Principal%'
-        AND job_title NOT LIKE '%Manager%'
-),
-
-total_jobs AS (
-    SELECT COUNT(DISTINCT job_id) AS total
-    FROM filtered_jobs
+WITH total_remote_postings AS (
+    SELECT 
+        COUNT(job_id) AS total_count
+    FROM 
+        job_postings_fact
+    WHERE 
+        job_title_short = 'Data Analyst' 
+        AND job_work_from_home = TRUE
 )
 
 SELECT 
     sd.skills,
-    COUNT(DISTINCT sj.job_id) AS demand_count,
-    COUNT(DISTINCT sj.job_id) * 100.0 / tj.total AS demand_percentage
-FROM filtered_jobs fj
-JOIN skills_job_dim sj 
-    ON fj.job_id = sj.job_id
-JOIN skills_dim sd 
-    ON sj.skill_id = sd.skill_id
-CROSS JOIN total_jobs tj
-GROUP BY sd.skills, tj.total
-ORDER BY demand_count DESC
+    COUNT(sjd.job_id) AS demand_count,
+    ROUND(COUNT(sjd.job_id) * 100.0 / (SELECT total_count FROM total_remote_postings), 2) AS demand_percentage
+FROM 
+    job_postings_fact AS jp
+INNER JOIN skills_job_dim AS sjd 
+    ON jp.job_id = sjd.job_id
+INNER JOIN skills_dim AS sd 
+    ON sjd.skill_id = sd.skill_id
+WHERE 
+    jp.job_title_short = 'Data Analyst' 
+    AND jp.job_work_from_home = TRUE
+GROUP BY 
+    sd.skills
+ORDER BY 
+    demand_count DESC
 LIMIT 5;
